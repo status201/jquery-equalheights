5c4bd2fe79b44c97eb90da07529c8cd66f540947
Gijs Oliemans <gijs@hyperconnected.nl>
2015-10-24 15:43:05 +0200

    [OTHER] NPM version bump

--- jquery.equalheights.js
+++ jquery.equalheights.js
@@ -1,6 +1,6 @@
 /**
  * Equal heights little jQuery plugin by Status201
- * @version 1.0.2
+ * @version 1.0.3
  * @requires jQuery 1.7+
  * @author Gijs Oliemans <gijs[at]status201.nl>
  * @license MIT
