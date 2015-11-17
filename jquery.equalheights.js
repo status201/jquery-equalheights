6227a577f895e80a1e39cab2e2506b02d7641198
Gijs Oliemans <gijs.oliemans@gmail.com>
2015-11-17 13:07:43 +0100

    Version bump

--- jquery.equalheights.js
+++ jquery.equalheights.js
@@ -1,6 +1,6 @@
 /**
  * Equal heights little jQuery plugin by Status201
- * @version 1.0.3
+ * @version 1.0.4
  * @requires jQuery 1.7+
  * @author Gijs Oliemans <gijs[at]status201.nl>
  * @license MIT
